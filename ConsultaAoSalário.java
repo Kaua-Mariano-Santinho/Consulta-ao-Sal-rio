3e0e4a7012337879888b76a084e294838fc9e199
Kaua-Mariano-Santinho <kauams.senai@gmail.com>
2025-02-21 15:32:59 -0300

    COMMIT inicial

--- ConsultaAoSalário.java
+++ ConsultaAoSalário.java
@@ -9,18 +9,23 @@ class Program {
         System.out.println("Sistema da Empresa X");
 
         // Declaração de variáveis
+        String nome;
         double salario;
         double salarioi;
+        int imposto;
 
         // Entrada
-        System.out.println("Olá funcionário, informe seu salário:");
+        System.out.println("Informe seu nome:");
+        nome = input.nextLine();
+        System.out.println("Olá " + nome + ", informe seu salário:");
         salario = input.nextDouble();
 
         // Processamento
         if (salario >= 5000) {
+            imposto = (int) (salario * 0.2);
             salarioi = salario * 0.8;
             System.out.println("Seu salário teve 20% de imposto retirado.");
-            System.out.println("Seu salário atual é: R$" + salarioi);
+            System.out.println("Seu salário atual é: R$" + salarioi + ". E o imposto pago foi de: R$" + imposto);
 
             // Saída caso tenha sido recolhido imposto
         } else {
